e5429b666944856b07a3b3463ec321a8594a1ca6
rovinsanque0 <rovinsanque0@gmail.com>
2025-11-07 02:08:47 -0600

    Add Review model and create_reviews migration

---
+++ db/migrate/20251107070034_create_reviews.rb
@@ -0,0 +1,12 @@
+class CreateReviews < ActiveRecord::Migration[7.2]
+  def change
+    create_table :reviews do |t|
+      t.references :restaurant, null: false, foreign_key: true
+      t.integer :rating
+      t.text :comment
+      t.string :author
+
+      t.timestamps
+    end
+  end
+end
