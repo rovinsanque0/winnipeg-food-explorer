0a7b9f91d27ef621763c7af9c658acabb24306f7
rovinsanque0 <rovinsanque0@gmail.com>
2025-11-07 02:06:40 -0600

    Initial Rails skeleton and configuration

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+# config/routes.rb
+Rails.application.routes.draw do
+  root "home#index"
+  get :about, to: "home#about"
+
+  resources :wards, only: %i[index show]
+  resources :categories, only: %i[index show]
+  resources :restaurants, only: %i[index show]
+end
